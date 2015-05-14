55869ff4ddaffcc9d52f523a356d596c487f35cc
Slacker87 <ccnp_pro@yahoo.com>
2015-05-14 02:39:20 +0000

    add nokogiri

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'open-uri'
+require 'nokogiri'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
