3e509bf7d414edeae9d7a3e3286d58648b9af60e
Slacker87 <ccnp_pro@yahoo.com>
2015-05-14 20:35:33 +0000

    start saving gif urls

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -4,7 +4,7 @@ class Gif < ActiveRecord::Base
   has_many :teamgifs
   
   validates :url, presence: true
-  validates :word, presence: true
+  #validates :word, presence: true
   
   def self.getgif(searchfor)
     return nil unless searchfor.present?
@@ -12,6 +12,15 @@ class Gif < ActiveRecord::Base
     url = "http://www.reactiongifs.com/?s=#{encodedsearch}&submit=Search"
     doc = Nokogiri::HTML(open(url))
     findgifs = doc.css('div.entry p a').map { |link| link['href'] }
+    #save found gifs if they are new
+    findgifs.each do |x|
+        if Gif.find_by_url(x).nil?
+            gif = Gif.new
+            gif.url = x
+            gif.save!
+        end
+    end
     findgifs
   end
+  
 end
