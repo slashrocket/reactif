e3215fa582a391e4826bdbfb90ac3b0baa4a9a72
Slacker87 <ccnp_pro@yahoo.com>
2015-05-14 18:14:11 +0000

    install devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,3 +11,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20150514181257) do
+
+  create_table "gifs", force: :cascade do |t|
+    t.string   "word"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "gifs", ["url"], name: "index_gifs_on_url"
+  add_index "gifs", ["word"], name: "index_gifs_on_word"
+
+  create_table "teamgifs", force: :cascade do |t|
+    t.integer  "gif_id"
+    t.integer  "team_id"
+    t.integer  "votes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "teamgifs", ["gif_id"], name: "index_teamgifs_on_gif_id"
+  add_index "teamgifs", ["team_id"], name: "index_teamgifs_on_team_id"
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+end
