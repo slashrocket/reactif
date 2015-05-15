e6a3fe11fb057d08a5968a173a75fd62614d5751
Slacker87 <ccnp_pro@yahoo.com>
2015-05-15 01:40:40 +0000

    try and clean up search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,51 +11,27 @@ class SearchController < ApplicationController
   end
 
   def slack
-    find_gifs_for(params[:text])
-    @text = params[:text]
+    @text = params[:text].downcase
     @channel = params[:channel_name]
     @username = params[:user_name]
-    @command = params[:command]
+    @domain = params[:team_domain]
+    find_gifs_for(@text, @domain, @channel, @username)
     @random_image = @found_images.sample
-
     @found_images.present? ? slack_response_gifs : no_gifs
   end
 
   private
 
-  def slack_response_gifs
-    @response_link = ' /reactif ' + @text + '    <' + @random_image + '?' + Random.rand(500).to_s + '|' + @random_image + '>'
-    @response_channel = '#' + @channel
-    HTTParty.post(SLACK_WEBHOOK_URL,
-                  body: {
-      payload: {
-        username: @username,
-        channel: @response_channel,
-        text: @response_link
-      }.to_json
-    })
-    render nothing: true
-  end
-
-  def no_gifs
-    render json: 'No gifs found'
-  end
-
-  def find_gifs_for(query)
-    @found = Gif.getgif(params[:text])
-    @text = params[:text].downcase
-    @channel = params[:channel_name]
-    @username = params[:user_name]
-    @command = params[:command]
-    @domain = params[:team_domain]
-    if @text == 'upvote'
-      Teamgif.upvote(id, @domain)
-    elsif @text == 'downvote'
-      Teamgif.downvote(id, @domain)
+  def find_gifs_for(query, domain, channel, username)
+    if query == 'upvote'
+      Teamgif.upvote(id, domain)
+    elsif query == 'downvote'
+      Teamgif.downvote(id, domain)
     end
-    @random_image = @found.sample
-    if @found.present?
-      post_gif_to_slack @random_image, @text, @channel, @username
+    found = Gif.getgif(query)
+    random_image = found.sample
+    if found.present? ? post_gif_to_slack(random_image, query, channel, username) : no_gifs
+      post_gif_to_slack random_image, query, channel, username
     else
       return render json: "No gifs found"
     end
@@ -65,7 +41,7 @@ class SearchController < ApplicationController
 def post_gif_to_slack(image, text, channel, username)
   responselink = " /reactif " + text + "    <" + image + "?" + Random.rand(500).to_s + "|" + image + ">"
   responsechannel = "#" + channel
-  HTTParty.post(ENV['SLACK_WEBHOOK_URL'],
+  HTTParty.post(SLACK_WEBHOOK_URL,
   {
     body: {
       payload: {
@@ -76,6 +52,10 @@ def post_gif_to_slack(image, text, channel, username)
     }
     })
   return render :nothing => true
+end
+
+def no_gifs
+  return render json: 'No gifs found'
 end
 
 def vote(channel, id)
