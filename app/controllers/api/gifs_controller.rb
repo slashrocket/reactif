4b3acf158bfff8f4844fba526a15e7b61503f51b
Daniel Climent <stokp1@gmail.com>
2015-05-28 19:49:23 +0300

    Authenticate API using the token stored in the user class

--- app/controllers/api/gifs_controller.rb
+++ app/controllers/api/gifs_controller.rb
@@ -2,7 +2,12 @@ module Api
   class GifsController < BaseController
 
     def show
-      render json: Gif.getgifs(params[:q]).sample.to_json
+      user = User.find_by_token(params[:token])
+      if user && ActiveSupport::SecurityUtils.secure_compare(user.token, params[:token])
+        render json: Gif.getgifs(params[:q]).sample.to_json
+      else
+        render status: 401, json: { success: false, info: "Invalid or Missing"}
+      end
     end
   end
 end
