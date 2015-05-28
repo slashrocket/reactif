2c0b9fee73f343b0fb90cc2b680e62814e78e30b
Daniel James McMahon <danieljmcmahon@gmail.com>
2015-05-27 20:51:37 -0500

    Clean up whitespace and strings

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,12 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @user = current_user
-    @teamcount = Team.where("user_id = ?", @user.id).count
+    @teamcount = Team.where('user_id = ?', @user.id).count
   end
-  
-  def settings 
+
+  def settings
     @user = current_user
     @teams = current_user.teams
   end
